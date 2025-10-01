c1f40060bedf112d048442bca9501ef0cc464bd6
Olaoluwa Olapegba <laoluolapegba@gmail.com>
2025-10-01 23:06:19 +0100

    FhirIngestAcceptedResponse correction

--- Ship.Ses.Ingestor.Api/src/Ship.Ses.Ingestor.WebApi/Models/FhirIngestAcceptedResponse.cs
+++ Ship.Ses.Ingestor.Api/src/Ship.Ses.Ingestor.WebApi/Models/FhirIngestAcceptedResponse.cs
@@ -3,7 +3,7 @@
     public sealed class FhirIngestAcceptedResponse
     {
         public string Status { get; init; } = "accepted";
-        //public string ResourceType { get; init; } = default!;
+        public string ResourceType { get; init; } = default!;
         public string? ResourceId { get; init; }
         public string? CorrelationId { get; init; }
         public DateTime Timestamp { get; init; } = DateTime.UtcNow;
